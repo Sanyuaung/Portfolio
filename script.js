b1c117fb7cd9965ccbe641b5f2b16468a723c180
San Yu Aung <sanyuaung.ygn.mm@gmail.com>
2023-08-11 11:40:02 +0630

    delete success

--- script.js
+++ script.js
@@ -122,11 +122,3 @@ scrollBottom.forEach((el)=>observer.observe(el));
 
 const scrollTop = document.querySelectorAll(".scroll-top");
 scrollTop.forEach((el)=>observer.observe(el));
-
-
-function redirectToSuccess() {
-  setTimeout(function() {
-    window.location.href = "https://sanyuaung.vercel.app/success.html";
-  }, 3000); 
-}
-window.onload = redirectToSuccess;
